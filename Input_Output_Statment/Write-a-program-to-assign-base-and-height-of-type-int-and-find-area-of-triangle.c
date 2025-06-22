a10a23f16462b9001dcd12349e1c4cd1a87fc11e
Khushi Yeligondar <kyeligondar@gmail.com>
2025-06-22 11:42:04 +0530

    Write-a-program-to-assign-base-and-height-of-type-int-and-find-area-of-triangle.c

---
+++ Input_Output_Statment/Write-a-program-to-assign-base-and-height-of-type-int-and-find-area-of-triangle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int base, height;
+    float Area;
+
+    base = 5;
+    height = 10;
+    Area = 1.0 / 2 * base * height;
+
+    printf("Area of Triangle = %f\n", Area);
+
+    return 0;
+}
