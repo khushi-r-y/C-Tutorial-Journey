3eb4bf84d978dca9af30ad8fef6e3a0934393365
Khushi Yeligondar <kyeligondar@gmail.com>
2025-06-22 11:44:16 +0530

    Calculate-product-of-two-integers.c

---
+++ Input_Output_Statment/Calculate-product-of-two-integers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, product;
+
+    a = 5;
+    b = 10;
+    product = a * b;
+
+    printf("Product of 2 integers = %d\n", product);
+
+    return 0;
+}
